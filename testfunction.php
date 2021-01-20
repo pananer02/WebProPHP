83627209be29b7f40df9216ac40a56070ec81702
pananer02 <pananer02@hotmail.com>
2021-01-20 11:03:20 +0700

    test

--- testfunction.php
+++ testfunction.php
@@ -1,7 +1,7 @@
 <?php
 $x = 100;
 $y = 10;
-$operator = "/";
+$operator = '/';
 
 function plus($x ,$y){
     $z = $x + $y;
@@ -19,6 +19,8 @@ function div($x ,$y){
     $z = $x / $y;
     echo "x / y = ",$z ,"<br>" ;
 }
+
+/*
 if($operator == "+"){
     plus($x,$y);
 }
@@ -31,5 +33,21 @@ else if($operator == "*"){
 else if($operator == "/"){
     div($x,$y);
 }
-
+*/
+switch ($operator){
+    case("+"):
+        plus($x,$y);
+        break;
+    case("-"):
+        minus($x,$y);
+        break;
+    case("*"):
+        mul($x,$y);
+        break;
+    case("/"):
+        div($x,$y);
+        break;
+    default:
+        
+}
 ?>
